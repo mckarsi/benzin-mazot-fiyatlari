fb04d20006d6e1b6388a58b40c380d0fb914f155
Mert Can Karşı <karsi.mertcan@gmail.com>
2014-08-18 15:52:58 +0300

    t

--- app/src/main/java/com/inndeks/benzinmazotfiyatlari/activity/MainActivity.java
+++ app/src/main/java/com/inndeks/benzinmazotfiyatlari/activity/MainActivity.java
@@ -1,18 +1,42 @@
 package com.inndeks.benzinmazotfiyatlari.activity;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.widget.ListView;
 
 import com.inndeks.benzinmazotfiyatlari.R;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import adapter.FuelListAdapter;
 import fragment.FragmentListing;
 import fragment.FragmentNoConnection;
+import model.FuelModel;
 
 
 public class MainActivity extends Activity {
@@ -25,6 +49,7 @@ public class MainActivity extends Activity {
         if (savedInstanceState == null) {
             if(checkInternetAvailability(context)){
                 getFragmentManager().beginTransaction().add(R.id.container, new FragmentListing()).commit();
+                new LoadFuelPrices().execute((Void) null);
             }else{
                 getFragmentManager().beginTransaction().add(R.id.container, new FragmentNoConnection()).commit();
             }
@@ -61,4 +86,77 @@ public class MainActivity extends Activity {
             return false;
         return true;
     }
+
+    private class LoadFuelPrices extends AsyncTask<Void, Void, Void> {
+
+        ProgressDialog progressDialog = null;
+        InputStream inputStream = null;
+        String jsonData = "";
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            progressDialog = new ProgressDialog(MainActivity.this);
+            progressDialog.setTitle("Lütfen Bekleyiniz");
+            progressDialog.setMessage("Piyasa verilieri güncelleniyor...");
+            progressDialog.setIndeterminate(false);
+            progressDialog.show();
+        }
+
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+
+                Long callbackName = (long)(Math.random()*9999999);
+                String url = "http://api.piyasa.com/jsonp/?callback=myCallBack" + callbackName.toString() + "&kaynak=akaryakit_guncel_Adıyaman";
+                Log.d("url", url);
+                HttpClient httpClient = new DefaultHttpClient();
+                HttpPost httpPost = new HttpPost(url);
+                //httpPost.setEntity(new UrlEncodedFormEntity(param));
+                HttpResponse httpResponse = httpClient.execute(httpPost);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                inputStream = httpEntity.getContent();
+                BufferedReader bReader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+                String line;
+                while ((line = bReader.readLine()) != null) {
+                    jsonData += line + "\n";
+                }
+                jsonData = jsonData.substring(jsonData.indexOf("(") + 1, jsonData.lastIndexOf(")")); // burada "jsonp to json" yaptım yani başındaki callback ifadesini sildim
+                Log.d("jsonData", jsonData);
+                inputStream.close();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void args) {
+            //parse JSON data
+            try {
+                List<FuelModel> fuelList = new ArrayList<FuelModel>();
+                FuelModel fuelModel;
+                JSONArray jArray = new JSONArray(jsonData);
+                for(int i=0; i < jArray.length(); i++) {
+                    fuelModel = new FuelModel();
+                    JSONObject jObject = jArray.getJSONObject(i);
+                    fuelModel.setFuelName(jObject.getString("source") + ":" + jObject.getString("fuel"));
+                    fuelModel.setFuelPrice(jObject.getString("price") + " TL");
+                    fuelList.add(fuelModel);
+                }
+                ListView fuelListObject = (ListView) findViewById(R.id.fuel_list);
+                FuelListAdapter listAdapter = new FuelListAdapter(MainActivity.this, R.layout.fuel_list_row, fuelList);
+                fuelListObject.setAdapter(listAdapter);
+                this.progressDialog.dismiss();
+            } catch (JSONException e) {
+                Log.e("JSONException", "Error: " + e.toString());
+            }
+        }
+
+    }
+
 }
--- app/src/main/java/fragment/FragmentListing.java
+++ app/src/main/java/fragment/FragmentListing.java
@@ -2,27 +2,12 @@ package fragment;
 
 import android.app.Fragment;
 import android.os.Bundle;
-import android.os.Environment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
 
 import com.inndeks.benzinmazotfiyatlari.R;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-
-import adapter.FuelListAdapter;
-import model.FuelModel;
 
 
 /**
@@ -30,59 +15,12 @@ import model.FuelModel;
  */
 public class FragmentListing  extends Fragment{
 
-    private List<FuelModel> fuelList;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 
         RelativeLayout layout = (RelativeLayout)inflater.inflate(R.layout.fragment_listing, container, false);
 
-        fuelList = downloadFuelPrices();
-        ListView fuelListObject = (ListView) layout.findViewById(R.id.fuel_list);
-        FuelListAdapter listAdapter = new FuelListAdapter(getActivity(), R.layout.fuel_list_row, fuelList);
-        fuelListObject.setAdapter(listAdapter);
-
         return layout;
-    }
-
-    private List<FuelModel> downloadFuelPrices(){
-        fuelList = new ArrayList<FuelModel>();
-
-        try {
-            URL url = new URL("http://api.piyasa.com/jsonp/?callback=OLUSTURULACAK_CALLBACK_DEGISKENI_RANDOM&kaynak=akaryakit_guncel_Adıyaman");
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            urlConnection.connect();
-            File SDCardRoot = Environment.getExternalStorageDirectory();
-            File file = new File(SDCardRoot,"akaryakit.xml");
-            FileOutputStream fileOutput = new FileOutputStream(file);
-            InputStream inputStream = urlConnection.getInputStream();
-            int downloadedSize = 0;
-            byte[] buffer = new byte[1024];
-            int bufferLength = 0; //used to store a temporary size of the buffer
-            while ( (bufferLength = inputStream.read(buffer)) > 0 ) {
-                //add the data in the buffer to the file in the file output stream (the file on the sd card
-                fileOutput.write(buffer, 0, bufferLength);
-                //add up the size so we know how much is downloaded
-                downloadedSize += bufferLength;
-
-            }
-            fileOutput.close();
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-
-        FuelModel fuelItem;
-
-        for(int i = 0 ; i<5; i++){
-            fuelItem =  new FuelModel();
-            fuelItem.setFuelName("benzin");
-            fuelItem.setFuelPrice("hgghh");
-            fuelList.add(fuelItem);
-        }
-
-        return fuelList;
     }
 
 }
